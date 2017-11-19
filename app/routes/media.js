7dedfa7d8372a777199103f6c6103a74255b1a2f
Ben L <benlertlumprasertkul@gmail.com>
2017-11-19 19:06:32 +0000

    add initial media albums with horizontal scroll

--- app/routes/media.js
+++ app/routes/media.js
@@ -1,4 +1,42 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  model() {
+    return [
+      {
+        title: 'Temple Photos',
+        photos: [
+          'assets/images/media/temple-photos/1486544107716.jpg',
+          'assets/images/media/temple-photos/1486544320624.jpg',
+          'assets/images/media/temple-photos/1486544467452.jpg',
+          'assets/images/media/temple-photos/1486544535766.jpg',
+          'assets/images/media/temple-photos/1486544581094.jpg',
+          'assets/images/media/temple-photos/1486544710262.jpg',
+          'assets/images/media/temple-photos/1486544765295.jpg',
+          'assets/images/media/temple-photos/1486544795774.jpg',
+          'assets/images/media/temple-photos/1486920501795.jpg',
+        ]
+      },
+      {
+        title: 'Meditation Photos',
+        photos: [
+          'assets/images/media/meditation-photos/591012-meditation cl_236.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_623.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_1080.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_1803.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_1919.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_3136.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_3479.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_4269.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_5292.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_5625.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_5775.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_8325.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_8370.jpg',
+          'assets/images/media/meditation-photos/591012-meditation cl_8659.jpg',
+        ]
+      },
+    ];
+  }
 });
