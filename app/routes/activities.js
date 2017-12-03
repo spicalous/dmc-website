28b2839d286b1b09ebef68a665bf06cb34906033
Ben L <benlertlumprasertkul@gmail.com>
2017-12-03 14:04:16 +0000

    added some card to activities

--- app/routes/activities.js
+++ app/routes/activities.js
@@ -1,4 +1,24 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+
+  model() {
+    return {
+      bhuddistCeremonies: [
+        {
+          cardImage: 'assets/images/activities/dhammachai-dhutanga_card.jpg',
+          title: 'DHAMMACHAI DHUTANGA',
+          shortDescription: 'Month-long mindfulness training',
+          link: 'activities'
+        },
+        {
+          cardImage: 'assets/images/activities/magha-puja_card.jpg',
+          title: 'MAGHA PUJA',
+          shortDescription: 'Inner light leads to a brighter world',
+          link: 'activities'
+        }
+      ]
+    }
+  }
+
 });
