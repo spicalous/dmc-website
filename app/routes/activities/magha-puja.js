74a42b337ac7629dca3a1ea82967f6c9749d956c
Ben L <benlertlumprasertkul@gmail.com>
2018-02-26 22:58:10 +0000

    magha puja content

---
+++ app/routes/activities/magha-puja.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+  actions: {
+
+    didTransition() {
+      window.scrollTo(0, 0);
+      return true;
+    }
+
+  }
+});
