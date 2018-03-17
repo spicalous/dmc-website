4773b56e1bbe8ea630b25bf1bcad9309e4427460
Ben L <benlertlumprasertkul@gmail.com>
2018-03-17 15:31:46 +0000

    move image down

--- app/components/img-banner.js
+++ app/components/img-banner.js
@@ -11,12 +11,16 @@ export default Component.extend({
     let styles = [`background-image: url('${this.get('src')}');`];
     const backgroundPosition = this.get('backgroundPosition');
     const contain = this.get('contain');
+    const noRepeat = this.get('noRepeat');
 
     if (backgroundPosition) {
       styles.push(`background-position: ${backgroundPosition};`)
     }
     if (contain) {
       styles.push(`background-size: contain;`)
+    }
+    if (noRepeat) {
+      styles.push(`background-repeat: no-repeat;`)
     }
 
     this.set('style', htmlSafe(styles.join('')));
