fb5b88655776ca270f9c711cb834b86fc58310ec
Ben Lertlumprasertkul <benlertlumprasertkul@gmail.com>
2019-05-12 19:37:39 +0100

    update environment config for gh-pages

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'dmc-website',
     environment,
-    rootURL: '/',
+    rootURL: '/dmc-website',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
